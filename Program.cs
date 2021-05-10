fb95357c8db3d46f012bb5797a81bf4a4567227e
Ninny659 <jordansamcooke@gmail.com>
2021-05-10 14:28:13 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NimGame Nim = new NimGame();
+            Nim.GameOfNim();
+        }
+
+    }
+}
---
+++ Renderer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Renderer
+    {
+        /*
+        Rederering Elements of the Game
+        */
+
+        private int[,] _gameBoard;
+
+        public Renderer(int[,] GameBoard)
+        {
+            this._gameBoard = GameBoard;   
+        }
+
+        public void WriteToConsole(int i) //Simple code to write to console 
+        {
+            Console.Clear();
+
+            if (i == 1)
+            {
+                Console.WriteLine("Player " + (i) + " turn\n---------------------------- -\nRows | Column\n----------------------------"); // The player is playing currently
+            }
+            else 
+            {
+                Console.WriteLine("Computers turn\n---------------------------- -\nRows | Column\n----------------------------"); // Its the Computers Turn to play
+            }
+            
+
+            for (int j = 0; j != _gameBoard.GetLength(0); j++)
+            {
+                WriteGameConsole(j);
+            }
+        }
+
+        public void WriteGameConsole(int row)
+        {
+            for (int j = 0; j < _gameBoard.GetLength(1); j++) Console.Write(string.Format("|{0}| ", _gameBoard[row, j])); // printing in in matrix 
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("----------------------------");
+        }
+
+
+    }
+}
